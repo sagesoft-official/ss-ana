b0b1a605b802afa50ad5dde177d048cb9a7a1e42
狐日泽 <shdocter@nya-wsl.com>
2024-03-30 14:24:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,45 +5,32 @@ from nicegui import ui
 
 version = "v0.0.1"
 
+def file_not_found_error(file):
+    file = str(file).replace("data/", "").replace(".json", "")
+    ana_messages = [{
+        "id": "error",
+        "msg": f"语录{file}不存在！",
+        "author": "None",
+        "time": "None"
+}]
+    return ana_messages
+
 @ui.page("/ana/a")
 def ana_page():
-    def file_not_found_error(file):
-        file = str(file).replace("data/", "").replace(".json", "")
-        ana_messages = [{
-            "id": "error",
-            "msg": f"语录{file}不存在！",
-            "author": "None",
-            "time": "None"
-    }]
-        return ana_messages
-
-    def get_ana(file):
-        if not os.path.exists(file):
-            ana_messages = file_not_found_error(file)
-            ana_status = False
-            return ana_status, ana_messages
-        else:
-            # asyncio.run(load_ana(file))
-            with open(file, 'r') as f:
-                ana_messages = json.load(f)
-            number = random.randint(0, len(ana_messages) - 1)
-            print(number)
-            ana_messages = ana_messages[number]
-            ana_status = True
-            return ana_status, ana_messages
-
-    ana = get_ana("data/a.json")
-
-    if not ana[0]:
-        id = ana[1][0]["id"]
-        msg = ana[1][0]["msg"]
-        author = ana[1][0]["author"]
-        time = ana[1][0]["time"]
+    file = "data/a.json"
+    if not os.path.exists(file):
+        ana_messages = file_not_found_error(file)
     else:
-        id = ana[1]["id"]
-        msg = ana[1]["msg"]
-        author = ana[1]["author"]
-        time = ana[1]["time"]
+        with open(file, 'r') as f:
+            ana_messages = json.load(f)
+        number = random.randint(0, len(ana_messages) - 1)
+        print(number)
+        ana_messages = ana_messages[number]
+
+    id = ana_messages["id"]
+    msg = ana_messages["msg"]
+    author = ana_messages["author"]
+    time = ana_messages["time"]
 
     with ui.card().classes("absolute-center"):
         ui.badge(f"桑尾草原语录 | ss_ana {version}", outline=True)
@@ -59,6 +46,11 @@ def ana_page():
         with ui.row():
             ui.badge("时间")
             ui.label(time)
+
+@ui.page("/ana/a/json")
+def ana_json_page():
+    func_vars = ana_page.__code__.co_cellvars
+    print(func_vars)
 
 with ui.card().classes("absolute-center"):
     ui.badge(f"桑尾草原语录 | ss_ana {version}", outline=True)
